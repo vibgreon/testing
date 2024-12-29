85f6f1f58d15940fa4bb750841b44598e268307d
vibgreon <vivekvenkatesh1234@gmail.com>
2024-12-29 07:30:13 +0530

    add nav and pages

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import "./Navbar.scss";
+ import { NavLink } from "react-router-dom";
 
 import { useState } from "react";
 
@@ -8,12 +9,12 @@ export default function Navbar() {
     <>
       <div className="nav__cont">
         <div className="nav_menu_cont">
-          <div
-            className={click ? "pointer mail_highlight" : "pointer"}
+          <a href="vivekvenkatesh1234@gmail.com"><div
+            className={click ? "mail_highlight" : ""}
             title="mail address"
           >
             vivekvenkatesh1234@gmail.com
-          </div>
+          </div></a>
           <div
             className="nav__menu"
             onClick={() => {
@@ -28,22 +29,23 @@ export default function Navbar() {
           </div>
         </div>
         <div className="nav__subcont">
-          {/* <div className="nav__item">work</div> */}
-          <div className="nav__item">about</div>
-          <div
-            className={`${"nav__item"} ${"pointer"}`}
+          <NavLink to={"/"}><div className="nav__item">Home</div></NavLink>
+          <NavLink to={"/about"}><div className="nav__item">about</div></NavLink>
+          <a href="https://drive.google.com/file/d/1vmfhK_7Zb-RnlbX46QHOWcVjKdTP3d7y/view?usp=sharing"><div
+            className={"nav__item resume"}
             title="google drive link"
           >
             <img src="./logo/google_drive.svg" />
             resume
-          </div>
-          <div className="nav__item">contact</div>
+          </div></a>
+          <NavLink to={"/contact"}><div className={"nav__item btn"}>contact</div></NavLink>
         </div>
         <div className={click ? "nav__fs" : "nav__fs_close nav__fs"}>
           <div>
-            <div>About</div>
-            <div>Resume</div>
-            <div>Contact</div>
+            <NavLink to={"/"}><div>Home</div></NavLink>
+            <NavLink to={"/about"}><div>About</div></NavLink>
+            <a href="https://drive.google.com/file/d/1vmfhK_7Zb-RnlbX46QHOWcVjKdTP3d7y/view?usp=sharing"><div>Resume</div></a>
+            <NavLink to={"/contact"}><div>Contact</div></NavLink>
           </div>
         </div>
       </div>
