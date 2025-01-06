723cbbf1a8572c1ec14aaa0dc6973d4cb2ee0f64
vibgreon <vivekvenkatesh1234@gmail.com>
2025-01-06 22:34:14 +0530

    UPDATE: Contact and Navbar element

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -7,6 +7,10 @@ export default function Contact() {
     <>
       <Wrapper>
         <div className="contact_cont">
+          <div className="contact_head">
+            <div className="header">Contact</div>
+            <div className="desc">Lets inspire each other. Glad to hear your story.</div>
+          </div>
           <div className="contact_subcont">
             <a href="https://www.linkedin.com/in/vibgreon/" target="_blank">
               <div className="contact_item">
