7f25065f53570b5653c662e9bb3b1c4a9e567d2f
vibgreon <vivekvenkatesh1234@gmail.com>
2025-07-26 20:20:02 +0530

    ADD: Read next blog recommendation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import ScrollToTop from "./components/hooks/ScrollToTop";
 
@@ -14,12 +14,15 @@ import Explaination from "./components/blog/Explaination/Explaination";
 import Snack from "./components/blog/Snack/Snack";
 import Breif from "./components/blog/Breif/Breif";
 
+import BlogReco from "./components/footer/BlogReco";
+
 import WorkSamples from "./pages/WorkSamples.json";
 var data = WorkSamples;
 
 import "./App.scss";
 
-function App() {
+function AppContent() {
+  const location = useLocation();
   const [installPrompt, setInstallPrompt] = useState(null);
 
   useEffect(() => {
@@ -41,49 +44,64 @@ function App() {
     }
   };
 
+  // Blog route matching logic
+  const blogPaths = data
+    .filter((item) => item.show === true)
+    .map((item) => `/${item.url}`);
+  const isBlogPage = blogPaths.includes(location.pathname);
+
   return (
-    <>
-      <BrowserRouter>
-        <ScrollToTop>
-          {installPrompt && <button onClick={installPWA}>Install App</button>}
-          <div className="app-nav__container">
-            <Wrapper>
-              <Navbar />
-            </Wrapper>
-          </div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            {data.filter((item) => item.show === true).map((item) => {
-              return (
-                <Route
-                  key={item.id}
-                  path={`/${item.url}`}
-                  element={(() => {
-                    switch (item.pageType) {
-                      case "explaination":
-                        return <Explaination data={item} />;
-                      case "snack":
-                        return <Snack data={item} />;
-                      case "breif":
-                        return <Breif data={item} />;
-                      default:
-                        return null;
-                    }
-                  })()}
-                />
-              );
-            })}
-          </Routes>
-          <Wrapper>
-            <Footer />
-          </Wrapper>
-        </ScrollToTop>
-      </BrowserRouter>
-    </>
+    <ScrollToTop>
+      {installPrompt && <button onClick={installPWA}>Install App</button>}
+
+      <div className="app-nav__container">
+        <Wrapper>
+          <Navbar />
+        </Wrapper>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        {data
+          .filter((item) => item.show === true)
+          .map((item) => {
+            return (
+              <Route
+                key={item.id}
+                path={`/${item.url}`}
+                element={(() => {
+                  switch (item.pageType) {
+                    case "explaination":
+                      return <Explaination data={item} />;
+                    case "snack":
+                      return <Snack data={item} />;
+                    case "breif":
+                      return <Breif data={item} />;
+                    default:
+                      return null;
+                  }
+                })()}
+              />
+            );
+          })}
+      </Routes>
+
+      <Wrapper>
+        {/* Only show BlogReco on blog pages */}
+        {isBlogPage && <BlogReco data={data} />}
+        <Footer />
+      </Wrapper>
+    </ScrollToTop>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+}
