7d577c04ae060f180b70b6f24ad4810747441fe0
vibgreon <vivekvenkatesh1234@gmail.com>
2026-04-14 23:23:30 +0530

    ADD: nav arrow

--- src/components/nav/NewNav.jsx
+++ src/components/nav/NewNav.jsx
@@ -5,8 +5,16 @@ import "./NewNav.scss";
 
 const slideUp = {
   initial: { opacity: 0, y: 12 },
-  animate: { opacity: 1, y: 0, transition: { duration: 0.35, ease: [0.33, 1, 0.68, 1] } },
-  exit: { opacity: 0, y: -12, transition: { duration: 0.25, ease: [0.32, 0, 0.67, 0] } },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.35, ease: [0.33, 1, 0.68, 1] },
+  },
+  exit: {
+    opacity: 0,
+    y: -12,
+    transition: { duration: 0.25, ease: [0.32, 0, 0.67, 0] },
+  },
 };
 
 export default function NewNav() {
@@ -24,11 +32,31 @@ export default function NewNav() {
       <AnimatePresence mode="wait">
         {isPlayground ? (
           <motion.div key="home" {...slideUp}>
-            <Link {...cursorProps("Home >")} to="/">Home</Link>
+            <Link {...cursorProps("Home >")} to="/" className="home">
+              Home
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M6 4L10 8L6 12"
+                  stroke="black"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </Link>
           </motion.div>
         ) : (
           <motion.div key="linkedin" {...slideUp}>
-            <a {...cursorProps("Social link >")} href="https://www.linkedin.com/in/vibgreon/" target="_self">
+            <a
+              {...cursorProps("Social link >")}
+              href="https://www.linkedin.com/in/vibgreon/"
+              target="_self"
+            >
               LinkedIn
             </a>
           </motion.div>
@@ -44,7 +72,11 @@ export default function NewNav() {
       <AnimatePresence>
         {!isPlayground && (
           <motion.div key="resume" {...slideUp}>
-            <a {...cursorProps("Google drive >")} href="https://drive.google.com/file/d/1zhfRYTeu0HPbh-jrEcYlYnG5lXxdDChd/view?usp=sharing" target="_self">
+            <a
+              {...cursorProps("Google drive >")}
+              href="https://drive.google.com/file/d/1zhfRYTeu0HPbh-jrEcYlYnG5lXxdDChd/view?usp=sharing"
+              target="_self"
+            >
               Resume
             </a>
           </motion.div>
